167d13cab0741ecb82a7429495eedd1ec10beaca
DerionbA <alemdavis8@gmail.com>
2024-11-13 17:05:10 -0400

    data type

---
+++ data type.py
@@ -0,0 +1,21 @@
+name = "Derion"
+age = "12"
+student = True
+weight = 1.5
+
+print ("name :", name)
+print ("data type is " , type(name))
+print ("age :", age)
+print ("data type is ",  type(age))
+print ("student :", student)
+print ("data type is " , type(student))
+print ("weight:", weight)
+print ("data type is " ,  type(weight))
+
+print("\n after")
+age = str (age)
+print(age)
+print("data type", type(age))
+weight= int (weight)
+print(weight)
+print("data type", type(weight))
